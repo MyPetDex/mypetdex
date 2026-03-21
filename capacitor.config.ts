8f3c4830d0a0c395e7d407b058362539157e0335
John Shafik <mypetdexapp@gmail.com>
2026-03-20 21:50:02 -0400

    add forgot password and account deletion

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.mypetdex',
+  appName: 'MyPetDex',
+  webDir: 'build'
+};
+
+export default config;
